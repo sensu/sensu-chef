816fa2bf42f023e58340e2f58e4daebdc0397c1f
Sean Porter <portertech@gmail.com>
2012-04-19 12:58:25 -0700

    [bunchr] update attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,28 @@
-# most of these values can be replaced with your method of discovery
+## most of these values can be replaced with your method of discovery
+
+# installation
 default.sensu.version = "0.9.5"
-default.sensu.installation = "rubygems"
+default.sensu.plugin.version = "0.1.3"
 default.sensu.directory = "/etc/sensu"
 default.sensu.log.directory = "/var/log/sensu"
-default.sensu.user = "sensu"
+
+# rabbitmq
 default.sensu.rabbitmq.host = "localhost"
 default.sensu.rabbitmq.port = 5671
 default.sensu.rabbitmq.ssl = Mash.new
 default.sensu.rabbitmq.vhost = "/sensu"
 default.sensu.rabbitmq.user = "sensu"
 default.sensu.rabbitmq.password = "password"
+
+# redis
 default.sensu.redis.host = "localhost"
 default.sensu.redis.port = 6379
+
+# api
 default.sensu.api.host = "localhost"
 default.sensu.api.port = 4567
-default.sensu.client = Hash.new
-default.sensu.dashboard.version = "0.9.7"
+
+# dashboard
 default.sensu.dashboard.host = "localhost"
 default.sensu.dashboard.port = 8080
 default.sensu.dashboard.user = "admin"
