ea816fc4d87cf8367e370c2e71841ff3631bb373
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-23 08:42:37 -0700

    Apply updates to sync with latest version.

--- libraries/sensu.rb
+++ libraries/sensu.rb
@@ -1,21 +1,16 @@
 module Sensu
   def self.generate_config(node, databag)
-    address = node.has_key?(:ec2) ? node.ec2.public_ipv4 : node.ipaddress
+    address = node.has_key?(:cloud) ? node.cloud.public_ipv4 : node.ipaddress
     node_config = Mash.new(
       node.sensu.to_hash.reject{ |key, value|
-        %w(user version).include?(key)
+        %w(id chef_type data_bag).include?(key)
       }
     )
     client_config = Mash.new(
       'client' => {
         :name => node.name,
         :address => address,
-        :subscriptions =>  [
-          node.roles,
-          node.recipes,
-          node.tags,
-          node.chef_environment
-        ].flatten.uniq
+        :subscriptions => node.roles
       }
     )
     databag_config = Mash.new(
@@ -31,16 +26,5 @@ module Sensu
         databag_config
       )
     )
-  end
-
-  def self.find_bin(service)
-    bin_path = "/usr/bin/sensu-#{service}"
-    ENV['PATH'].split(':').each do |path|
-      test_path = File.join(path, "sensu-#{service}")
-      if File.exists?(test_path)
-        bin_path = test_path
-      end
-    end
-    bin_path
   end
 end
