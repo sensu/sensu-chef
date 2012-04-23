6568a4f22e37d1eada0716fbb370550cac9d00a3
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-23 08:09:13 -0700

    Use deep merge when merging node attributes with data bag

--- libraries/sensu.rb
+++ libraries/sensu.rb
@@ -1,13 +1,46 @@
 module Sensu
   def self.generate_config(node, databag)
-    config = node.sensu.to_hash.reject { |key, value| %w[plugin directory log sudoers firewall].include?(key) }
-    address = node.attribute?(:cloud) ? node.cloud.public_ipv4 : node.ipaddress
-    config['client'].merge!(
-      :name => node.name,
-      :address => address,
-      :subscriptions => node.roles
+    address = node.has_key?(:ec2) ? node.ec2.public_ipv4 : node.ipaddress
+    node_config = Mash.new(
+      node.sensu.to_hash.reject{ |key, value|
+        %w(user version).include?(key)
+      }
     )
-    config.merge!(databag.reject { |key, value| %w[id chef_type data_bag].include?(key) })
-    JSON.pretty_generate(config)
+    client_config = Mash.new(
+      'client' => {
+        :name => node.name,
+        :address => address,
+        :subscriptions =>  [
+          node.roles,
+          node.recipes,
+          node.tags,
+          node.chef_environment
+        ].flatten.uniq
+      }
+    )
+    databag_config = Mash.new(
+      databag.reject{ |key,value|
+        %w(id chef_type data_bag).include?(key)
+      }
+    )
+    JSON.pretty_generate(
+      Chef::Mixin::DeepMerge.merge(
+        Chef::Mixin::DeepMerge.merge(
+          node_config, client_config
+        ),
+        databag_config
+      )
+    )
+  end
+
+  def self.find_bin(service)
+    bin_path = "/usr/bin/sensu-#{service}"
+    ENV['PATH'].split(':').each do |path|
+      test_path = File.join(path, "sensu-#{service}")
+      if File.exists?(test_path)
+        bin_path = test_path
+      end
+    end
+    bin_path
   end
 end
