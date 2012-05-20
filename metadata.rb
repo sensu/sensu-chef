47513654fd39f610ba50d24902f367262e91b5e5
Sean Porter <portertech@gmail.com>
2012-05-20 01:10:03 -0700

    version bump, prepare for community site

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer       "Sonian, Inc."
 maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.7"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.8"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
