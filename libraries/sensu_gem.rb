a07598bd20ef2b4445c37e20b012160e47f7974a
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-26 12:39:31 -0700

    Add sensu_gem resource

---
+++ libraries/sensu_gem.rb
@@ -0,0 +1,13 @@
+class Chef::Resource::SensuGem < Chef::Resource::GemPackage
+  def initialize(name, run_context=nil)
+    super
+    @resource_name = :sensu_gem
+    @provider = Chef::Provider::Package::Rubygems
+  end
+
+  def gem_binary
+    if(::File.exists?('/opt/sensu/embedded/bin/gem'))
+      '/opt/sensu/embedded/bin/gem'
+    end
+  end
+end
