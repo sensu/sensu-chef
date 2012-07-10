2c0d6790fc1ef2682bdf38b91b810448a7a85e56
Sean Porter <portertech@gmail.com>
2012-07-09 22:39:28 -0700

    [sensu-0.9.6] bump sensu version default attribute to 0.9.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 ## most of these values can be replaced with your method of discovery
 
 # installation
-default.sensu.version = "0.9.5-36"
+default.sensu.version = "0.9.6-1"
 default.sensu.plugin.version = "0.1.3"
 default.sensu.directory = "/etc/sensu"
 default.sensu.log.directory = "/var/log/sensu"
