9ceb6e9af7d68a952c87fa14f2a3acab6722b3fd
Sean Porter <portertech@gmail.com>
2012-08-11 08:20:48 -0700

    [init] sensu init scripts support status and restart

--- recipes/api.rb
+++ recipes/api.rb
@@ -21,6 +21,7 @@ include_recipe "sensu::default"
 
 service "sensu-api" do
   provider node.platform =~ /ubuntu|debian/ ? Chef::Provider::Service::Init::Debian : Chef::Provider::Service::Init::Redhat
+  supports :status => true, :restart => true
   action [:enable, :start]
   subscribes :restart, resources(:sensu_config => node.name), :delayed
 end
