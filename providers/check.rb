3f2256a09f2248abc0d2edec9a1a3b9cba5fc7e3
Sean Porter <portertech@gmail.com>
2012-10-30 23:02:23 -0700

    [fix-lwrps] check and handler providers need parent dirs

--- providers/check.rb
+++ providers/check.rb
@@ -7,7 +7,14 @@ action :create do
     }
   }
 
-  json_file ::File.join(node.sensu.directory, "conf.d", "checks", "#{new_resource.name}.json") do
+  checks_directory = ::File.join(node.sensu.directory, "conf.d", "checks")
+
+  directory checks_directory do
+    recursive true
+    mode 0755
+  end
+
+  json_file ::File.join(checks_directory, "#{new_resource.name}.json") do
     content definition
     mode 0644
     notifies :create, "ruby_block[sensu_service_trigger]", :immediately
