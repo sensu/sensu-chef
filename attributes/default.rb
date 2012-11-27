f2ab7e0081b3267c1f1b196c32eea10378c06162
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-11-26 16:23:41 -0800

    New sensu::admin recipe, no application dependencies

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,5 +27,21 @@ default.sensu.dashboard.port = 8080
 default.sensu.dashboard.user = "admin"
 default.sensu.dashboard.password = "secret"
 
+# admin
+default.sensu.admin.host = "localhost"
+default.sensu.admin.http_port = 80
+default.sensu.admin.https_port = 443
+default.sensu.admin.backend_port = 8888
+default.sensu.admin.repo = "https://github.com/sensu/sensu-admin.git"
+default.sensu.admin.release = "v0.0.3" # Version locked, if you want the latest use development, if you want some stability use master. YMMV.
+default.sensu.admin.base_path = "/opt/sensu/admin" # Omnibus sensu lives here
+default.sensu.admin.ssl.country = 'US'
+default.sensu.admin.ssl.state = 'MA'
+default.sensu.admin.ssl.city = 'Boston'
+default.sensu.admin.ssl.company = 'MyCo, Inc'
+default.sensu.admin.ssl.email = 'chefs@mydomain.net'
+default.sensu.admin.ssl.domain = 'mydomain.net'
+default.sensu.admin.ssl.department = 'DEVOPS'
+
 # client
 default.sensu.client = Hash.new
