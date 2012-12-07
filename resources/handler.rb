0f31f979295458caea8fe71343a252ac0a09c4b8
Sean Porter <portertech@gmail.com>
2012-12-06 17:19:30 -0800

    [filters] added "filters" to handler lwrp

--- resources/handler.rb
+++ resources/handler.rb
@@ -1,12 +1,13 @@
 actions :create, :delete
 
-attribute :type, :kind_of => String
+attribute :type, :kind_of => String, :equal_to => %w[set pipe tcp udp amqp]
+attribute :filters, :kind_of => Array
+attribute :mutator, :kind_of => String
+attribute :severities, :kind_of => Array
+attribute :handlers, :kind_of => Array
 attribute :command, :kind_of => String
 attribute :socket, :kind_of => Hash
 attribute :exchange, :kind_of => Hash
-attribute :severities, :kind_of => Array
-attribute :handlers, :kind_of => Array
-attribute :mutator, :kind_of => String
 attribute :additional, :kind_of => Hash, :default => Hash.new
 
 def initialize(*args)
