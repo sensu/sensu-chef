f014032613a1e1f28e70daa5f0340961503be2e2
Sean Porter <portertech@gmail.com>
2012-12-06 17:05:03 -0800

    [filters] filter lwrp, removed check lwrp subscribers and standalone defaults

---
+++ providers/filter.rb
@@ -0,0 +1,22 @@
+action :create do
+  definition = {
+    "filters" => {
+      new_resource.name => new_resource.to_hash.reject { |key, value|
+        !%w[attributes negate].include?(key.to_s)
+      }
+    }
+  }
+
+  filters_directory = ::File.join(node.sensu.directory, "conf.d", "filters")
+
+  directory filters_directory do
+    recursive true
+    mode 0755
+  end
+
+  json_file ::File.join(filters_directory, "#{new_resource.name}.json") do
+    content definition
+    mode 0644
+    notifies :create, "ruby_block[sensu_service_trigger]", :immediately
+  end
+end
---
+++ resources/filter.rb
@@ -0,0 +1,9 @@
+actions :create
+
+attribute :attributes, :kind_of => Hash, :required => true
+attribute :negate, :kind_of => [TrueClass, FalseClass]
+
+def initialize(*args)
+  super
+  @action = :create
+end
