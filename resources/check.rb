a1439fd9367021a65e9f5e1ac93bb6bb9c2f9cf0
Sean Porter <portertech@gmail.com>
2012-12-06 16:53:55 -0800

    [filters] check lwrp type possible values

--- resources/check.rb
+++ resources/check.rb
@@ -1,12 +1,12 @@
 actions :create, :delete
 
+attribute :type, :kind_of => String, :equal_to => %w[status metric]
 attribute :command, :kind_of => String, :required => true
 attribute :subscribers, :kind_of => Array, :default => Array.new
 attribute :standalone, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :interval, :default => 60
 attribute :handlers, :kind_of => Array
 attribute :additional, :kind_of => Hash, :default => Hash.new
-attribute :type, :kind_of => String, :default => nil
 
 def initialize(*args)
   super
