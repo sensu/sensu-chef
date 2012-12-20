bbe9f360e5f5cdd11664047ed723c6d4225502a5
Cameron Johnston <cameron@needle.com>
2012-12-20 12:35:39 -0700

    first pass at sensu_api_stash LWRP

---
+++ libraries/sensu_api.rb
@@ -0,0 +1,73 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+module Sensu
+  class API
+    class Stash
+      def initialize(api_uri)
+        @server_uri = URI(api_uri)
+      end
+
+      def get(path)
+        req = Net::HTTP::Get.new(path)
+        response = Net::HTTP.start(@server_uri.host, @server_uri.port) do |http|
+          http.request(req)
+        end
+
+        case response.code
+        when '200'
+          # stash exists, so load the response body into @current_resource.payload
+          # but omit the timestamp; @new_resource.timestamp should always differ
+          body = JSON.parse(response.body)
+          body.reject {|k,v| k == 'timestamp'}
+        when '404'
+          false
+        when '500'
+          Chef::Log.fatal "Sensu::API::Stash: Error getting #{path} from #{api_uri}: " + response.body.inspect
+          raise
+        end 
+      end
+
+      def post(path,payload)
+        req = Net::HTTP::Post.new(path, {'Content-Type'=>'application/json'})
+
+        payload = payload.merge({
+         'timestamp' => Time.now.to_i,
+         'actor' => 'chef'
+        }).to_json
+
+        req.body = payload
+
+        response = Net::HTTP.start(@server_uri.host, @server_uri.port) do |http|
+          http.request(req)
+        end
+
+        case response.code
+        when '201'
+          Chef::Log.debug "Sensu::API::Stash: Successfully stashed payload to #{path} on #{@server_uri}: " + payload.inspect
+        when '400'
+          Chef::Log.fatal "Sensu::API::Stash: Malformed payload for #{path} on #{@server_uri}: " + payload.inspect
+        when '500'
+          Chef::Log.fatal "Sensu::API::Stash: Error stashing payload to #{path} on #{@server_uri}: " + response.inspect
+        end
+      end
+
+      def delete(path)
+        req = Net::HTTP::Delete.new(path)
+        response = Net::HTTP.start(@server_uri.host, @server_uri.port) do |http|
+          http.request(req)
+        end
+
+        case response.code
+        when '204'
+          Chef::Log.debug "Sensu::API::Stash: Successfully deleted stash #{path} on #{@server_uri}."
+        when '404'
+          Chef::Log.debug "Sensu::API::Stash: Could not delete stash #{path} on #{@server_uri}, as it does not exist."
+        when '500'
+          Chef::Log.fatal "Sensu::API::Stash: Error deleting stash #{path} on #{@server_uri}: " + response.body.inspect
+        end
+      end
+    end
+  end
+end
---
+++ providers/api_stash.rb
@@ -0,0 +1,24 @@
+action :create do
+  api = Sensu::API::Stash.new(@new_resource.api_uri)
+  unless @new_resource.payload == @current_resource.payload
+    if api.post("/stashes/#{@new_resource.name}",@new_resource.payload)
+      @new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
+action :delete do
+  api = Sensu::API::Stash.new(@new_resource.api_uri)
+  unless @current_resource.payload == false
+    if api.delete("/stashes/#{@new_resource.name}")
+      @new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
+def load_current_resource
+  @current_resource = Chef::Resource::SensuApiStash.new(@new_resource.name)
+  @current_resource.api_uri(@new_resource.api_uri)
+  api = Sensu::API::Stash.new(@current_resource.api_uri)
+  @current_resource.payload(api.get("/stashes/#{@current_resource.name}"))
+end
