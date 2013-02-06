31bf5c005a9ae60798455f3317fec2631ae8ec46
Sean Porter <portertech@gmail.com>
2013-02-06 11:03:02 -0800

    [json_file] module methods

--- libraries/sensu_json_file.rb
+++ libraries/sensu_json_file.rb
@@ -1,17 +1,19 @@
-class SensuJsonFile
-  def load_json(path)
-    JSON.parse(File.read(path)) rescue Hash.new
-  end
+module SensuJsonFile
+  class << self
+    def load_json(path)
+      JSON.parse(File.read(path)) rescue Hash.new
+    end
 
-  def dump_json(obj)
-    JSON.pretty_generate(obj) + "\n"
-  end
+    def dump_json(obj)
+      JSON.pretty_generate(obj) + "\n"
+    end
 
-  def to_mash(obj)
-    Mash.from_hash(obj)
-  end
+    def to_mash(obj)
+      Mash.from_hash(obj)
+    end
 
-  def compare_content(path, content)
-    to_mash(load_json(path)) == to_mash(content)
+    def compare_content(path, content)
+      to_mash(load_json(path)) == to_mash(content)
+    end
   end
 end
