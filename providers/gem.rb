28516a91cd18db5e90375a21b0de6af5ee35a208
Sean Porter <portertech@gmail.com>
2013-02-06 21:51:38 -0800

    [sensu_gem] some gem provider cleanup

--- providers/gem.rb
+++ providers/gem.rb
@@ -1,30 +1,23 @@
 action :install do
-    name = @new_resource.name
-    gem_version = @new_resource.version
-    gem_options = @new_resource.options
-    gem_package name do
-      if ::File.exists?('/opt/sensu/embedded/bin/gem')
-        gem_binary("/opt/sensu/embedded/bin/gem")
-      else
-        gem_binary("gem")
-      end
-      version gem_version
-      options gem_options
+  gem_package new_resource.name do
+    if File.exists?("/opt/sensu/embedded/bin/gem")
+      gem_binary "/opt/sensu/embedded/bin/gem"
+    else
+      gem_binary "gem"
     end
+    version new_resource.version
+    options new_resource.options
+  end
 end
 
 action :remove do
-    name = @new_resource.name
-    gem_version = @new_resource.version
-    gem_options = @new_resource.options
-    gem_package name do
-      if ::File.exists?('/opt/sensu/embedded/bin/gem')
-        gem_binary("/opt/sensu/embedded/bin/gem")
-      else
-        gem_binary("gem")
-      end
-      version gem_version
-      options gem_options
-      action :remove
+  gem_package new_resource.name do
+    if File.exists?("/opt/sensu/embedded/bin/gem")
+      gem_binary "/opt/sensu/embedded/bin/gem"
+    else
+      gem_binary "gem"
     end
+    version new_resource.version
+    action :remove
+  end
 end
