f9f73ff6f65ceaa50e9fa167ee7c0fb2d26963f1
@agent462 <agent462@gmail.com>
2013-02-06 23:48:41 -0600

    this makes it backwards compatible

--- providers/base_config.rb
+++ providers/base_config.rb
@@ -1,10 +1,10 @@
 action :create do
   type = new_resource.type
   definitions = node.sensu.to_hash.reject do |key, value|
-    if type == 'server'
-      !%w[rabbitmq redis api dashboard].include?(key.to_s) || value.nil?
-    else
+    if type == 'client'
       !%w{rabbitmq}.include?(key.to_s) || value.nil?
+    else
+      !%w[rabbitmq redis api dashboard].include?(key.to_s) || value.nil?
     end
   end
   sensu_json_file ::File.join(node.sensu.directory, "config.json") do
