662d391f4bdb36de9a3914e7fa010444e3b428c1
Cameron Johnston <cameron@needle.com>
2013-04-23 13:20:18 -0600

    [runit] updating sensu_service lwrp to make use of init_scheme param

--- providers/service.rb
+++ providers/service.rb
@@ -15,21 +15,41 @@ def load_current_resource
 end
 
 action :enable do
-  ruby_block "block_until_runsv_#{new_resource.name}_available" do
-    block do
-      Chef::Log.debug("waiting until named pipe #{service_pipe} exists")
-      until ::FileTest.pipe?(service_pipe)
-        sleep(1)
-        Chef::Log.debug(".")
-      end
+  case new_resource.init_style
+  when "sysvinit"
+    service new_resource.name do
+      provider node.platform_family =~ /debian/ ? Chef::Provider::Service::Init::Debian : Chef::Provider::Service::Init::Redhat
+      supports :status => true, :restart => true
+      action [:enable, :start]
+      subscribes :restart, resources("ruby_block[sensu_service_trigger]"), :delayed
     end
-    action :nothing
-  end
+  when "runit"
+    ruby_block "block_until_runsv_#{new_resource.name}_available" do
+      block do
+        Chef::Log.debug("waiting until named pipe #{service_pipe} exists")
+        until ::FileTest.pipe?(service_pipe)
+          sleep(1)
+          Chef::Log.debug(".")
+        end
+      end
+      action :nothing
+    end
 
-  execute "sensu-ctl_#{new_resource.name}_enable" do
-    command "#{sensu_ctl} #{new_resource.name} enable"
-    not_if { @service_enabled }
-    notifies :create, "ruby_block[block_until_runsv_#{new_resource.name}_available]", :immediately
+    execute "sensu-ctl_#{new_resource.name}_enable" do
+      command "#{sensu_ctl} #{new_resource.name} enable"
+      not_if { @service_enabled }
+      notifies :create, "ruby_block[block_until_runsv_#{new_resource.name}_available]", :immediately
+    end
+
+    service new_resource.name do
+      start_command "#{sensu_ctl} sensu-api start"
+      stop_command "#{sensu_ctl} sensu-api stop"
+      status_command "#{sensu_ctl} sensu-api status"
+      restart_command "#{sensu_ctl} sensu-api restart"
+      supports :restart => true, :status => true
+      action [:start]
+      subscribes :restart, resources("ruby_block[sensu_service_trigger]"), :delayed
+    end
   end
 end
 
