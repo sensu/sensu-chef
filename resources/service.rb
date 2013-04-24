3c1f9e241662262776f34b7a179e3cbdc72462fd
Cameron Johnston <cameron@needle.com>
2013-04-24 14:58:40 -0600

    [runit] prefer :equal_to over :regex for service lwrp

--- resources/service.rb
+++ resources/service.rb
@@ -1,7 +1,7 @@
 actions :enable, :disable
 
-attribute :service, :name_attribute => true, :kind_of => String, :required => true, :regex => /^(sensu-server|sensu-client|sensu-api|sensu-dashboard)$/
-attribute :init_style, :kind_of => String, :required => true, :regex => /^(sysv|runit)$/
+attribute :service, :name_attribute => true, :kind_of => String, :required => true, :equal_to => %w[sensu-server sensu-client sensu-api sensu-dashboard]
+attribute :init_style, :kind_of => String, :required => true, :equal_to => %w[sysv runit]
 
 def initialize(*args)
   super
