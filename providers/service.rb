720fa2e0114c715cdf1f3389aafb7b4cbf3b6821
Cameron Johnston <cameron@needle.com>
2013-04-24 08:02:21 -0600

    [runit] renaming service lwrp "name" parameter to "service" so regex will work

--- providers/service.rb
+++ providers/service.rb
@@ -3,11 +3,11 @@ def sensu_ctl
 end
 
 def service_pipe
-  "/opt/sensu/sv/#{new_resource.name}/supervise/ok"
+  "/opt/sensu/sv/#{new_resource.service}/supervise/ok"
 end
 
 def service_path
-  "/opt/sensu/sv/service/#{new_resource.name}"
+  "/opt/sensu/sv/service/#{new_resource.service}"
 end
 
 def load_current_resource
@@ -17,14 +17,14 @@ end
 action :enable do
   case new_resource.init_style
   when "sysv"
-    service new_resource.name do
+    service new_resource.service do
       provider node.platform_family =~ /debian/ ? Chef::Provider::Service::Init::Debian : Chef::Provider::Service::Init::Redhat
       supports :status => true, :restart => true
       action [:enable, :start]
       subscribes :restart, resources("ruby_block[sensu_service_trigger]"), :delayed
     end
   when "runit"
-    ruby_block "block_until_runsv_#{new_resource.name}_available" do
+    ruby_block "block_until_runsv_#{new_resource.service}_available" do
       block do
         Chef::Log.debug("waiting until named pipe #{service_pipe} exists")
         until ::FileTest.pipe?(service_pipe)
@@ -35,17 +35,17 @@ action :enable do
       action :nothing
     end
 
-    execute "sensu-ctl_#{new_resource.name}_enable" do
-      command "#{sensu_ctl} #{new_resource.name} enable"
+    execute "sensu-ctl_#{new_resource.service}_enable" do
+      command "#{sensu_ctl} #{new_resource.service} enable"
       not_if { @service_enabled }
-      notifies :create, "ruby_block[block_until_runsv_#{new_resource.name}_available]", :immediately
+      notifies :create, "ruby_block[block_until_runsv_#{new_resource.service}_available]", :immediately
     end
 
-    service new_resource.name do
-      start_command "#{sensu_ctl} #{new_resource.name} start"
-      stop_command "#{sensu_ctl} #{new_resource.name} stop"
-      status_command "#{sensu_ctl} #{new_resource.name} status"
-      restart_command "#{sensu_ctl} #{new_resource.name} restart"
+    service new_resource.service do
+      start_command "#{sensu_ctl} #{new_resource.service} start"
+      stop_command "#{sensu_ctl} #{new_resource.service} stop"
+      status_command "#{sensu_ctl} #{new_resource.service} status"
+      restart_command "#{sensu_ctl} #{new_resource.service} restart"
       supports :restart => true, :status => true
       action [:start]
       subscribes :restart, resources("ruby_block[sensu_service_trigger]"), :delayed
@@ -56,13 +56,13 @@ end
 action :disable do
   case new_resource.init_style
   when "sysv"
-    service new_resource.name do
+    service new_resource.service do
       provider node.platform_family =~ /debian/ ? Chef::Provider::Service::Init::Debian : Chef::Provider::Service::Init::Redhat
       action [:disable]
     end
   when "runit"
-    execute "sensu-ctl_#{new_resource.name}_disable" do
-      command "#{sensu_ctl} #{new_resource.name} disable"
+    execute "sensu-ctl_#{new_resource.service}_disable" do
+      command "#{sensu_ctl} #{new_resource.service} disable"
       only_if { @service_enabled }
     end
   end
--- resources/service.rb
+++ resources/service.rb
@@ -1,6 +1,6 @@
 actions :enable, :disable
 
-attribute :name, :kind_of => String, :required => true, :regex => /^(sensu-server|sensu-client|sensu-api|sensu-dashboard)$/
+attribute :service, :name_attribute => true, :kind_of => String, :required => true, :regex => /^(sensu-server|sensu-client|sensu-api|sensu-dashboard)$/
 attribute :init_style, :kind_of => String, :required => true, :regex => /^(sysv|runit)$/
 
 def initialize(*args)
