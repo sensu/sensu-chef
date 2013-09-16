edb789dd11332f41a65a0b29ee0192fc84cb34df
Pat Christopher <patrick.christopher@mtnsat.com>
2013-09-16 14:34:09 -0700

    allow setting of keepalive handler
    adds the keepalive keyword to the client LWRP so you can set a client
    specific handler for keepalive events

--- resources/client.rb
+++ resources/client.rb
@@ -3,6 +3,7 @@ actions :create
 attribute :address, :kind_of => String, :required => true
 attribute :subscriptions, :kind_of => Array, :default => Array.new
 attribute :additional, :kind_of => Hash, :default => Hash.new
+attribute :keepalive, :kind_of => Hash, :default => Hash.new
 
 def initialize(*args)
   super
