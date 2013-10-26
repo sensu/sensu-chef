83b726937d17f0185d47dfb74cebe4d100657dfe
Sean Porter <portertech@gmail.com>
2013-10-26 11:59:42 -0700

    bumped default version of sensu to 0.11.3-1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # installation
-default.sensu.version = "0.11.0-1"
+default.sensu.version = "0.11.3-1"
 default.sensu.use_unstable_repo = false
 default.sensu.directory = "/etc/sensu"
 default.sensu.log_directory = "/var/log/sensu"
