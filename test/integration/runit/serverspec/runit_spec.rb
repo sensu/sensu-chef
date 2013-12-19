5a5581e318970a8fcddca5dfefe8a99f0301b0b8
Sean Porter <portertech@gmail.com>
2013-12-18 19:37:47 -0800

    [encrypted] test runit sensu service restart

--- test/integration/runit/serverspec/runit_spec.rb
+++ test/integration/runit/serverspec/runit_spec.rb
@@ -12,17 +12,17 @@ RSpec.configure do |c|
 end
 
 describe file("/opt/sensu/sv/sensu-server/supervise/pid") do
-  its(:content) { should match /[0-9]+/ }
+  its(:content) { should match /^[0-9]+$/ }
 end
 
 describe file("/opt/sensu/sv/sensu-client/supervise/pid") do
-  its(:content) { should match /[0-9]+/ }
+  its(:content) { should match /^[0-9]+$/ }
 end
 
 describe file("/opt/sensu/sv/sensu-api/supervise/pid") do
-  its(:content) { should match /[0-9]+/ }
+  its(:content) { should match /^[0-9]+$/ }
 end
 
 describe file("/opt/sensu/sv/sensu-dashboard/supervise/pid") do
-  its(:content) { should match /[0-9]+/ }
+  its(:content) { should match /^[0-9]+$/ }
 end
