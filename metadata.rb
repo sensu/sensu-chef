3281925b48a811dedf3df3ae874b7aa97a203c54
Sean Porter <portertech@gmail.com>
2013-12-19 20:12:40 -0800

    minor version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.7.0"
+version          "0.7.1"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
