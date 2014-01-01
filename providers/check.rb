fb9def8b829f3e73f1ec29fbb8dd03bdd588d2f6
AJ Bourg <aj.bourg@sendgrid.com>
2013-12-31 20:53:56 -0700

    Add missing support for dependencies support

--- providers/check.rb
+++ providers/check.rb
@@ -10,7 +10,7 @@ action :create do
     new_resource,
     %w[
       type command timeout subscribers standalone aggregate handle
-      handlers publish low_flap_threshold high_flap_threshold
+      dependencies handlers publish low_flap_threshold high_flap_threshold
     ]
   ).merge("interval" => new_resource.interval).merge(new_resource.additional)
 
