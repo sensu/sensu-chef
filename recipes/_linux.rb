6aba5f8239a9bad71998f368b1bc6ffce16ae7de
nickm4062 <nick@nickmontgomery.com>
2014-10-07 20:19:57 -0700

    fix spelling of true

--- recipes/_linux.rb
+++ recipes/_linux.rb
@@ -67,7 +67,7 @@ end
 package "sensu" do
   version node.sensu.version
   options package_options
-  allow_downgrade trues
+  allow_downgrade true
   notifies :create, "ruby_block[sensu_service_trigger]"
 end
 
