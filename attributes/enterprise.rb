e1d71c30019d39f1d496fd669f205f34af331fe3
Sean Porter <portertech@gmail.com>
2014-12-17 13:14:22 -0800

    [enterprise] bumped the beta build iteration number

--- attributes/enterprise.rb
+++ attributes/enterprise.rb
@@ -1,5 +1,5 @@
 # installation
-default.sensu.enterprise.version = "1.0.0.beta.1-1"
+default.sensu.enterprise.version = "1.0.0.beta.1-2"
 default.sensu.enterprise.use_unstable_repo = false
 default.sensu.enterprise.log_level = "warn"
 default.sensu.enterprise.heap_size = "2048m"
