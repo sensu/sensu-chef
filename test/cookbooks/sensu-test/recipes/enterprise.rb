8e15ec80a8e43769faf7abf54e590e8302737c81
Sean Porter <portertech@gmail.com>
2014-12-18 02:21:03 -0800

    [enterprise_rhel] fixed enterprise rhel install

---
+++ test/cookbooks/sensu-test/recipes/enterprise.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: sensu-test
+# Recipe:: enterprise
+#
+# Copyright 2014, Heavy Water Operations, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "logrotate"
+
+include_recipe "sensu::default"
+
+include_recipe "sensu::rabbitmq"
+include_recipe "sensu::redis"
+include_recipe "sensu::enterprise"
+include_recipe "sensu::enterprise_service"
+
+# ServerSpec dependencies
+
+if platform?("ubuntu")
+  package "net-tools"
+end
