8a69e79e4df330e53f174df8d138ad9aea0a0caa
Paul Thomas <paul+github@paulthomas.eu>
2015-01-15 15:31:19 +0000

    tests for using vault for encryption

---
+++ test/integration/vault/serverspec/encrypted_spec.rb
@@ -0,0 +1,25 @@
+require "serverspec"
+require "net/http"
+require "uri"
+
+set :backend, :exec
+set :path, "/bin:/usr/bin:/sbin:/usr/sbin"
+
+describe service("sensu-server") do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service("sensu-client") do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service("sensu-api") do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/etc/sensu/config.json') do
+  its(:content) { should match /vaultpassword/ }
+end
