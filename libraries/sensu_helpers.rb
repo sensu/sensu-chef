4d21a968d065ee69aa9c424960e55698a2ab6ae0
Ryan Bezdicek <ryanjbezdicek@gmail.com>
2015-01-29 13:42:20 -0600

    Updated random_password function to ensure certain level of complexity is met due to occasianl issues with windows server

--- libraries/sensu_helpers.rb
+++ libraries/sensu_helpers.rb
@@ -58,10 +58,38 @@ module Sensu
         missing_ok ? nil : raise(error)
       end
 
-      def random_password(length=20)
+      def random_password(length=20, number=false, upper=false, lower=false, special=false)
         password = ""
-        while password.length < length
-          password << ::OpenSSL::Random.random_bytes(1).gsub(/\W/, '')
+        requiredOffset = 0
+        requiredOffset += 1 if number
+        requiredOffset += 1 if upper
+        requiredOffset += 1 if lower
+        requiredOffset += 1 if special
+        length = requiredOffset if length < requiredOffset
+        limit = password.length < (length - requiredOffset)
+
+        while limit || requiredOffset > 0
+          push = false
+          c = ::OpenSSL::Random.random_bytes(1).gsub(/\W/, '')
+          if c != ""
+            if c =~ /[[:digit:]]/
+              requiredOffset -= 1 if number
+              number = false
+            elsif c >= 'a' && c <= 'z'
+              requiredOffset -= 1 if lower
+              lower = false
+            elsif c >= 'A' && c <= 'Z'
+              requiredOffset -= 1 if upper
+              upper = false
+            else
+              requiredOffset -= 1 if special
+              special = false
+            end
+          end
+          limit = password.length < (length - requiredOffset)
+          if limit
+            password << c
+          end
         end
         password
       end
