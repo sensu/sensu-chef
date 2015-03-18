520e03fc810f34d0d583240654a205c521eea2dd
Sean Porter <portertech@gmail.com>
2015-03-17 18:31:00 -0700

    [sensu-0.17.0] deploy sensu 0.17.0-1 by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@ else
 end
 
 # installation
-default.sensu.version = "0.16.0-1"
+default.sensu.version = "0.17.0-1"
 default.sensu.use_unstable_repo = false
 default.sensu.log_level = "info"
 default.sensu.use_ssl = true
