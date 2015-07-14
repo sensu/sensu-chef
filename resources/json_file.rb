0574cdb247c8e0269ad64a5341ea69f45b53584e
Joshua Garnett <jgarnett@zynga.com>
2015-07-13 20:38:03 -0400

    Default group should be the group specified by the attributes

--- resources/json_file.rb
+++ resources/json_file.rb
@@ -2,7 +2,7 @@ actions :create, :delete
 
 attribute :path, :name_attribute => true
 attribute :owner, :kind_of => String
-attribute :group, :kind_of => String, :default => 'sensu'
+attribute :group, :kind_of => String, :default => node['sensu']['group']
 attribute :mode, :kind_of => [String, Integer], :default => 0640
 attribute :content, :kind_of => Hash
 
