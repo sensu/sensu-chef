b1c376b5109ccea0175b57d1ab20d6fc6b7dcd6c
Justin Kolberg <amd.prophet@gmail.com>
2015-09-22 13:01:14 -0700

    [dashboard] use enterprise_dashboard as default databag name

--- attributes/enterprise_dashboard.rb
+++ attributes/enterprise_dashboard.rb
@@ -3,7 +3,7 @@ default["sensu"]["enterprise-dashboard"]["version"] = "1:1.4.0-1"
 
 # data bag
 default["sensu"]["enterprise-dashboard"]["data_bag"]["name"] = "sensu"
-default["sensu"]["enterprise-dashboard"]["data_bag"]["config_item"] = "dashboard_config"
+default["sensu"]["enterprise-dashboard"]["data_bag"]["config_item"] = "enterprise_dashboard"
 
 # dashboard options
 default["sensu"]["enterprise-dashboard"]["dashboard"]["host"] = "0.0.0.0"
