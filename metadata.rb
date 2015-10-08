dfd9a3692572665cde13d0b05680b97a78d9c237
Jan Klare <j.klare@x-ion.de>
2015-10-08 12:55:28 +0200

    fix dependency for chef-vault in current stable release
    * new release of chef-vault 1.3.1 changed module name from ChefVaultItem to
      ChefVaultCookbook
    
    fixes #375

--- metadata.rb
+++ metadata.rb
@@ -22,7 +22,7 @@ depends "rabbitmq", ">= 2.0.0"
 depends "redisio", ">= 1.7.0"
 
 # available @ https://supermarket.chef.io/cookbooks/chef-vault
-suggests "chef-vault", ">= 1.2.0"
+suggests "chef-vault", "<= 1.3.0"
 
 %w[
   ubuntu
