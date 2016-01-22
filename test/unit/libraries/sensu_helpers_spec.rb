ae0414b213b7504ac89f365411c603e350e60a55
Cameron Johnston <cameron@rootdown.net>
2016-01-22 12:06:00 -0700

    adding unit coverage for Sensu::Helpers.gem_binary

--- test/unit/libraries/sensu_helpers_spec.rb
+++ test/unit/libraries/sensu_helpers_spec.rb
@@ -5,9 +5,9 @@ require_relative '../../../libraries/sensu_helpers.rb'
 describe Sensu::Helpers do
 
   let(:node) { Fauxhai.mock(:platform => 'ubuntu', :version => '14.04').data }
+  let(:omnibus_gem_path) { '/opt/sensu/embedded/bin/gem' }
 
   describe ".select_attributes" do
-
     context 'when the requested attribute exists' do
       it 'returns the requested key/value pair' do
         results = Sensu::Helpers.select_attributes(node, 'platform')
@@ -35,6 +35,31 @@ describe Sensu::Helpers do
         results = Sensu::Helpers.select_attributes(node, ['platform_version', 'platform_lasagna'])
         expect(results.keys).to eq(['platform_version'])
         expect(results.values).to eq(['14.04'])
+      end
+    end
+  end
+
+  describe ".gem_binary" do
+    context 'with omnibus ruby available' do
+      before do
+        allow(File).to receive(:exists?).with(omnibus_gem_path).and_return(true)
+      end
+
+      it 'returns the full path to the omnibus ruby gem binary' do
+        gem_binary = Sensu::Helpers.gem_binary
+        expect(gem_binary).to eq(omnibus_gem_path)
+      end
+    end
+
+    context 'without omnibus ruby available' do
+
+      before do
+        allow(File).to receive(:exists?).with(omnibus_gem_path).and_return(false)
+      end
+
+      it 'returns an unqualified path to the gem binary' do
+        gem_binary = Sensu::Helpers.gem_binary
+        expect(gem_binary).to eq('gem')
       end
     end
   end
