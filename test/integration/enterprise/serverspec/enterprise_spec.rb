e08ae7bef7be4f6463de09ad95c830024d8a5538
Cameron Johnston <cameron@rootdown.net>
2016-03-02 09:40:01 -0700

    remove sensu-client from enterprise specs, use shared helper and service dependency specs

--- test/integration/enterprise/serverspec/enterprise_spec.rb
+++ test/integration/enterprise/serverspec/enterprise_spec.rb
@@ -1,18 +1,7 @@
-require "net/http"
-require 'serverspec'
-require "uri"
-
-set :backend, :exec
-set :path, "/bin:/usr/bin:/sbin:/usr/sbin"
-
-puts "os: #{os}"
+require 'spec_helper'
+require 'service_dependency_spec'
 
 describe service("sensu-enterprise") do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service("sensu-client") do
   it { should be_enabled }
   it { should be_running }
 end
