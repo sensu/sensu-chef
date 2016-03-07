ee7c2b16075ceb02ccfeb6f785f5f4e7c8e4c5e2
Cameron Johnston <cameron@rootdown.net>
2016-03-07 11:07:56 -0700

    lazily evaluate sensu_json_file owner and group defaults from node attributes
    Prior to this change the default group for a sensu_json_file resource was
    hardcoded to 'sensu' as a string.
    
    In [#422](https://github.com/sensu/sensu-chef/pull/422) it is suggested to use the value of node['sensu']['group'] attribute to set this default, but the implementation in that PR is problematic due to the attribute being evaluated when the cookbook loads.
    
    In [#389](https://github.com/sensu/sensu-chef/pull/389) it is suggested to have the sensu_json_file resource manage the user and group for its owner and group, respectively. I'd rather not manage this aspect off the system from what's intended as a simple wrapper around the file resource.
    
    I believe that if we lazily evaluate the 'admin_user' and 'group' attributes as defaults for the sensu_json_file resource we will make it possible for folks to avoid having to create 'sensu' user/group when they are trying to test sensu_json_file resources in isolation.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe file("/etc/sensu/config.json") do
   its(:content) { should match /"name": "test"/ }
   its(:content) { should_not match /"id": / }
+  it { should be_grouped_into('nogroup') }
 end
