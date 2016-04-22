3650e78ab55f1a7df07a3f9c449b07f2854f7dca
Cameron Johnston <cameron@rootdown.net>
2016-04-22 12:55:43 -0600

    update serverspec helper with conditionals for windows

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -2,7 +2,31 @@ require "net/http"
 require 'serverspec'
 require "uri"
 
-set :backend, :exec
-set :path, "/bin:/usr/bin:/sbin:/usr/sbin"
+# h/t to @martinb3 for http://martinb3.io/testkitchen-serverspec-windows/
+if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
+  set :backend, :exec
+  set :path, "/bin:/usr/bin:/sbin:/usr/sbin:$PATH"
+else
+  set :backend, :cmd
+  set :os, :family => "windows"
+end
 
 puts "os: #{os}"
+
+# helper methods to conditionally guard individual tests by platform
+def linux?
+  %w(debian redhat ubuntu).include?(os[:family])
+end
+
+def windows?
+  %w(windows).include?(os[:family])
+end
+
+def sensu_directory
+  case windows?
+  when true
+    'C:\etc\sensu'
+  else
+    "/etc/sensu"
+  end
+end
