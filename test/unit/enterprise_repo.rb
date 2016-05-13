080758a406bf56fb2342d0697ebe9b25ebc5119c
Cameron Johnston <cameron@rootdown.net>
2016-05-13 15:20:29 -0600

    add unit tests for _enterprise_repo recipe

---
+++ test/unit/enterprise_repo.rb
@@ -0,0 +1,90 @@
+require_relative "spec_helper"
+
+describe "sensu::_enterprise_repo" do
+  let(:data_bag_item) do
+    {
+      "enterprise" => {
+        "repository" => {
+          "credentials" => {
+            "user" => "chefspec",
+            "password" => "moartests!"
+          }
+        }
+      }
+    }
+  end
+
+  %w(stable unstable).each do |repo_designation|
+    context "apt platforms" do
+      let(:chef_run) do
+        ChefSpec::ServerRunner.new(:platform => "ubuntu", :version => "12.04") do |node, server|
+          node.set["sensu"]["enterprise"]["use_unstable_repo"] = true unless repo_designation == "stable"
+          server.create_data_bag("sensu", data_bag_item)
+        end.converge(described_recipe)
+      end
+
+      context "using upstream #{repo_designation} repo" do
+        it "creates sensu-enterprise apt #{repo_designation} repository definition" do
+          expect(chef_run).to add_apt_repository("sensu-enterprise").with(
+            :uri => "http://chefspec:moartests!@enterprise.sensuapp.com/apt",
+            :key => "http://chefspec:moartests!@enterprise.sensuapp.com/apt/pubkey.gpg",
+            :distribution => "sensu-enterprise",
+            :components => repo_designation == "unstable" ?  ["unstable"] : ["main"]
+          )
+        end
+      end
+
+      context "using custom host with #{repo_designation} repo" do
+        before do
+          chef_run.node.set["sensu"]["enterprise"]["repo_host"] = "chefspec.example.com"
+          chef_run.converge(described_recipe)
+        end
+
+        it "creates sensu-enterprise yum #{repo_designation} repository definition using custom repo host" do
+          expect(chef_run).to add_apt_repository("sensu-enterprise").with(
+            :uri => "http://chefspec:moartests!@chefspec.example.com/apt",
+            :key => "http://chefspec:moartests!@chefspec.example.com/apt/pubkey.gpg",
+            :distribution => "sensu-enterprise",
+            :components => repo_designation == "unstable" ?  ["unstable"] : ["main"]
+          )
+        end
+      end
+    end
+
+    context "yum platforms" do
+
+      let(:chef_run) do
+        ChefSpec::ServerRunner.new(:platform => "centos", :version => "6.6") do |node, server|
+          node.set["sensu"]["enterprise"]["use_unstable_repo"] = true unless repo_designation == "stable"
+          server.create_data_bag("sensu", data_bag_item)
+        end.converge(described_recipe)
+      end
+
+      let(:yum_repo_designation) { repo_designation == "unstable" ? "yum-unstable" : "yum" }
+
+      context "using upstream #{repo_designation} repo" do
+        it "creates sensu-enterprise yum #{repo_designation} repository definition" do
+          expect(chef_run).to add_yum_repository("sensu-enterprise").with(
+            :url => "http://chefspec:moartests!@enterprise.sensuapp.com/#{yum_repo_designation}/noarch/",
+            :gpgcheck => false
+          )
+        end
+      end
+
+      context "using custom host with #{repo_designation} repo" do
+        before do
+          chef_run.node.set["sensu"]["enterprise"]["repo_host"] = "chefspec.example.com"
+          chef_run.converge(described_recipe)
+        end
+
+        it "creates sensu-enterprise yum #{repo_designation} repository definition using custom repo host" do
+          expect(chef_run).to add_yum_repository("sensu-enterprise").with(
+            :url => "http://chefspec:moartests!@chefspec.example.com/#{yum_repo_designation}/noarch/",
+            :gpgcheck => false
+          )
+        end
+      end
+    end
+
+  end
+end
