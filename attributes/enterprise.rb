76700b096225bac618b18ac37e7c085bdeecae53
Terry Corley <terrycorley@users.noreply.github.com>
2016-07-29 15:58:44 -0500

    Add JAVA_OPTS to sensu-enterprise default file (#463)

--- attributes/enterprise.rb
+++ attributes/enterprise.rb
@@ -5,3 +5,4 @@ default["sensu"]["enterprise"]["version"] = "1.5.2-1"
 default["sensu"]["enterprise"]["use_unstable_repo"] = false
 default["sensu"]["enterprise"]["log_level"] = "info"
 default["sensu"]["enterprise"]["heap_size"] = "2048m"
+default["sensu"]["enterprise"]["java_opts"] = ""
