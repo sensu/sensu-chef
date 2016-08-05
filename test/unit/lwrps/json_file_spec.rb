625f7c6c1ade9cf4c2b283346dcce4dee01caa7f
Cameron Johnston <cameron@rootdown.net>
2016-08-05 13:04:46 -0600

    [tests/unit] add test for pretty-printed JSON file in json_file_spec

--- test/unit/lwrps/json_file_spec.rb
+++ test/unit/lwrps/json_file_spec.rb
@@ -1,9 +1,8 @@
 require_relative "../spec_helper"
 
 describe 'sensu_json_file' do
-
+  let(:test_content) { { :lol => "wtfbbq" } }
   let(:test_directory_mode) { "0755" }
-
   let(:chef_run) do
     ChefSpec::SoloRunner.new(
       :step_into => ['sensu_json_file'],
@@ -15,6 +14,12 @@ describe 'sensu_json_file' do
 
   it 'creates the /etc/sensu directory using value of directory_mode attribute' do
     expect(chef_run).to create_directory('/etc/sensu').with({ :mode => test_directory_mode })
+  end
+
+  it 'creates a "pretty" json file with the provided content' do
+    expect(chef_run).to render_file('/etc/sensu/foo.json').with_content(
+      JSON.pretty_generate(test_content)
+    )
   end
 
 end
