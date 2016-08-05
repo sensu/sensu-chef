9acca0e770a7fcdca16461184c96863bf9e624ce
Cameron Johnston <cameron@rootdown.net>
2016-08-05 09:07:00 -0600

    [sensu_json_file] refactor resources to reduce CHEF-3694 warnings

--- providers/json_file.rb
+++ providers/json_file.rb
@@ -3,24 +3,23 @@ action :create do
     r.to_s == "ruby_block[sensu_service_trigger]"
   end
 
-  unless Sensu::JSONFile.compare_content(new_resource.path, new_resource.content)
-    directory ::File.dirname(new_resource.path) do
-      recursive true
-      owner lazy { new_resource.owner || node["sensu"]["admin_user"] }
-      group lazy { new_resource.group || node["sensu"]["group"] }
-      mode lazy { node["sensu"]["directory_mode"] }
-    end
-
-    f = file new_resource.path do
-      owner lazy { new_resource.owner || node["sensu"]["admin_user"] }
-      group lazy { new_resource.group || node["sensu"]["group"] }
-      mode new_resource.mode
-      content Sensu::JSONFile.dump_json(new_resource.content)
-      notifies :create, "ruby_block[sensu_service_trigger]", :delayed if sensu_service_trigger
-    end
-
-    new_resource.updated_by_last_action(f.updated_by_last_action?)
+  directory "create_dir_for_#{new_resource.path}" do
+    path ::File.dirname(new_resource.path)
+    recursive true
+    owner lazy { new_resource.owner || node["sensu"]["admin_user"] }
+    group lazy { new_resource.group || node["sensu"]["group"] }
+    mode lazy { node["sensu"]["directory_mode"] }
   end
+
+  f = file new_resource.path do
+    owner lazy { new_resource.owner || node["sensu"]["admin_user"] }
+    group lazy { new_resource.group || node["sensu"]["group"] }
+    mode new_resource.mode
+    content Sensu::JSONFile.dump_json(new_resource.content)
+    notifies :create, "ruby_block[sensu_service_trigger]", :delayed if sensu_service_trigger
+  end
+
+  new_resource.updated_by_last_action(f.updated_by_last_action?)
 end
 
 action :delete do
