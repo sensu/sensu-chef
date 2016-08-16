162c0b609c231f2d20b133d851dfcf5f4127c4f5
Will Salt <williamejsalt@gmail.com>
2016-08-16 15:09:51 +0100

    rename systemd spec file appropriately and test that systemd service unit file  is loaded

--- test/integration/systemd/serverspec/systemd_spec.rb
+++ test/integration/systemd/serverspec/systemd_spec.rb
@@ -10,6 +10,10 @@ describe service("sensu-server") do
   it { should be_running }
 end
 
+describe command('systemctl status sensu-server') do
+  its(:stdout) { should match %r{Loaded: loaded \(/etc/systemd/system/sensu-server.service;} }
+end
+
 describe file('/etc/init.d/sensu-client') do
   it { should_not exist }
 end
@@ -19,6 +23,10 @@ describe service("sensu-client") do
   it { should be_running }
 end
 
+describe command('systemctl status sensu-client') do
+  its(:stdout) { should match %r{Loaded: loaded \(/etc/systemd/system/sensu-client.service;} }
+end
+
 describe file('/etc/init.d/sensu-api') do
   it { should_not exist }
 end
@@ -26,4 +34,8 @@ end
 describe service("sensu-api") do
   it { should be_enabled }
   it { should be_running }
+end
+
+describe command('systemctl status sensu-api') do
+  its(:stdout) { should match %r{Loaded: loaded \(/etc/systemd/system/sensu-api.service;} }
 end
