19a18d3c3b7906d80b267ec407382057a283c352
Cameron Johnston <cameron@rootdown.net>
2016-11-23 15:11:15 -0700

    update template for sensu-enterprise defaults, manage heap dump directory

--- attributes/enterprise.rb
+++ attributes/enterprise.rb
@@ -6,3 +6,5 @@ default["sensu"]["enterprise"]["use_unstable_repo"] = false
 default["sensu"]["enterprise"]["log_level"] = "info"
 default["sensu"]["enterprise"]["heap_size"] = "2048m"
 default["sensu"]["enterprise"]["java_opts"] = ""
+default["sensu"]["enterprise"]["max_open_files"] = 16384
+default["sensu"]["enterprise"]["heap_dump_path"] = "/var/cache/sensu-enterprise"
--- test/integration/enterprise/serverspec/enterprise_spec.rb
+++ test/integration/enterprise/serverspec/enterprise_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 require 'service_dependency_helper'
 
+describe file("/var/cache/sensu-enterprise") do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by 'sensu' }
+  it { should be_grouped_into 'sensu' }
+end
+
 describe service("sensu-enterprise") do
   it { should be_enabled }
   it { should be_running }
