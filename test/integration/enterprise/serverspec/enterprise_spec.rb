a648d97afd82ba649bae58dcc2ebd0310e2247f5
Cameron Johnston <cameron@rootdown.net>
2017-01-09 12:39:22 -0700

    add integration test for se heap dump directory

--- test/integration/enterprise/serverspec/enterprise_spec.rb
+++ test/integration/enterprise/serverspec/enterprise_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 require 'service_dependency_helper'
 
-describe file("/var/cache/sensu-enterprise") do
+describe file("/etc/default/sensu-enterprise") do
+  it { should exist }
+  its(:content) { should include('/var/cache/sensu-enterprise-nondefault') }
+end
+
+describe file("/var/cache/sensu-enterprise-nondefault") do
   it { should exist }
   it { should be_directory }
   it { should be_owned_by 'sensu' }
