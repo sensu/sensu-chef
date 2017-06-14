e780b5ed72cb3560988ee3f18b95b3b300b62bbc
Tim Smith <tsmith@chef.io>
2017-06-14 09:30:37 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "sensu"
 maintainer       "Heavy Water Operations"
 maintainer_email "support@sensuapp.com"
-license          "Apache 2.0"
+license          "Apache-2.0"
 description      "Installs/Configures Sensu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "4.0.1"
