ce367e9465cfc2771185b8d942fb7acf936fcfea
thomasriley <thomasriley@users.noreply.github.com>
2017-12-16 00:24:30 +0000

    Change repos to use HTTPS

--- attributes/default.rb
+++ attributes/default.rb
@@ -31,10 +31,10 @@ default["sensu"]["log_directory_mode"] = "0750"
 default["sensu"]["client_deregister_on_stop"] = false
 default["sensu"]["client_deregister_handler"] = nil
 
-default["sensu"]["apt_repo_url"] = "http://repositories.sensuapp.org/apt"
-default["sensu"]["yum_repo_url"] = "http://repositories.sensuapp.org"
+default["sensu"]["apt_repo_url"] = "https://repositories.sensuapp.org/apt"
+default["sensu"]["yum_repo_url"] = "https://repositories.sensuapp.org"
 default['sensu']['yum_flush_cache'] = nil
-default["sensu"]["msi_repo_url"] = "http://repositories.sensuapp.org/msi"
+default["sensu"]["msi_repo_url"] = "https://repositories.sensuapp.org/msi"
 default["sensu"]["aix_package_root_url"] = "https://sensu.global.ssl.fastly.net/aix"
 default["sensu"]["add_repo"] = true
 
