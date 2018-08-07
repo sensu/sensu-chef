d0047001aa6e291e31b8a450bd3ee8d0cbc6cef6
S.Cavallo <smcavallo@hotmail.com>
2018-08-07 10:03:23 -0400

    add chefspec for all available amazon linux 2 platform versions Signed-off-by: S.Cavallo <smcavallo@hotmail.com>

--- spec/sensu-chef/_linux_spec.rb
+++ spec/sensu-chef/_linux_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'sensu::_linux' do
+  # Test all the versions available in fauxhai
+  # https://github.com/chefspec/fauxhai/tree/master/lib/fauxhai/platforms/amazon
   context 'amazon_linux_2' do
     let(:solo) do
-      ChefSpec::SoloRunner.new(:platform => 'amazon',
-                               :version => '2')
+      ChefSpec::SoloRunner.new(platform: 'amazon',
+                               version: '2')
     end
     let(:chef_run) do
       # We need to insert this into the run_context
@@ -20,29 +22,39 @@ describe 'sensu::_linux' do
       expect(chef_run).to install_yum_package('sensu').with(version: '1.2.0-1.el7')
     end
   end
-  context 'amazon_linux_2-pre' do
-    let(:solo) do
-      ChefSpec::SoloRunner.new(:platform => 'amazon',
-                               :version => '2018.03')
-    end
-    let(:chef_run) do
-      # We need to insert this into the run_context
-      # Because we aren't loading the recipe which includes this resource
-      solo.converge(described_recipe) do
-        solo.resource_collection.insert(
-            Chef::Resource::RubyBlock.new('sensu_service_trigger', solo.run_context)
-        )
+
+  ['2018.03',
+   '2017.09',
+   '2017.03',
+   '2016.09',
+   '2016.03',
+   '2015.09',
+   '2015.03'].each do |platform_version|
+    context 'amazon_linux_2-pre' do
+      let(:solo) do
+        ChefSpec::SoloRunner.new(platform: 'amazon',
+                                 version: platform_version)
+      end
+      let(:chef_run) do
+        # We need to insert this into the run_context
+        # Because we aren't loading the recipe which includes this resource
+        solo.converge(described_recipe) do
+          solo.resource_collection.insert(
+              Chef::Resource::RubyBlock.new('sensu_service_trigger', solo.run_context)
+          )
+        end
+      end
+      it 'runs the _linux recipe' do
+        expect(chef_run).to add_yum_repository('sensu').with(baseurl: 'http://repositories.sensuapp.org/yum/6/$basearch/')
+        expect(chef_run).to install_yum_package('sensu').with(version: '1.2.0-1.el6')
       end
     end
-    it 'runs the _linux recipe' do
-      expect(chef_run).to add_yum_repository('sensu').with(baseurl: 'http://repositories.sensuapp.org/yum/6/$basearch/')
-      expect(chef_run).to install_yum_package('sensu').with(version: '1.2.0-1.el6')
-    end
+
   end
   context 'ubuntu_bionic' do
     let(:solo) do
-      ChefSpec::SoloRunner.new(:platform => 'ubuntu',
-                               :version => '18.04')
+      ChefSpec::SoloRunner.new(platform: 'ubuntu',
+                               version: '18.04')
     end
     let(:chef_run) do
       # We need to insert this into the run_context
