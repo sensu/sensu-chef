80329e89ace6c2721202f35e23302e275037ca70
Ben Abrams <me@benabrams.it>
2018-09-14 15:34:18 -0700

    `sensu_gem` can now optionally specify `gem_package`
    This is helpful when you need multiple versions of the same plugin (or any gemfor that matter) by allowing the resource name and package name to differ.
    
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- providers/gem.rb
+++ providers/gem.rb
@@ -4,6 +4,7 @@ action :install do
     version new_resource.version
     source  new_resource.source
     options new_resource.options
+    package_name new_resource.package_name unless new_resource.package_name.nil?
   end
 
   new_resource.updated_by_last_action(g.updated_by_last_action?)
@@ -15,6 +16,7 @@ action :upgrade do
     version new_resource.version
     source  new_resource.source
     options new_resource.options
+    package_name new_resource.package_name unless new_resource.package_name.nil?
     action :upgrade
   end
 
@@ -25,6 +27,7 @@ action :remove do
   g = gem_package new_resource.name do
     gem_binary Sensu::Helpers.gem_binary
     version new_resource.version
+    package_name new_resource.package_name unless new_resource.package_name.nil?
     action :remove
   end
 
--- resources/gem.rb
+++ resources/gem.rb
@@ -4,3 +4,4 @@ default_action :install
 attribute :version, :kind_of => String
 attribute :source,  :kind_of => String
 attribute :options, :kind_of => [String, Hash]
+attribute :package_name, :kind_of => [String, NilClass], :default => nil
